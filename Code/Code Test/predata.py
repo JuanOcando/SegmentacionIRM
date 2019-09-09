2504c63862bab53aa914c341d5c446be225b38f3
Juan Ocando <13-10991@usb.ve>
2019-09-09 12:58:32 +0200

    Add files via upload

---
+++ Code/Code Test/predata.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  4 22:07:13 2019
+
+@author: OcandJua
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('patientData.csv')
+
